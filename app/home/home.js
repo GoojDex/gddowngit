68ea41bf73ee29df6cfad5678621006961bff217
GoojDex <114043672+GoojDex@users.noreply.github.com>
2023-03-19 11:38:26 +0530

    Update home.js

--- app/home/home.js
+++ app/home/home.js
@@ -32,7 +32,7 @@ homeModule.config([
 
                     var templateUrl = "https?://github.com/.+/.+";
                     var downloadUrlInfix = "#/home?url=";
-                    var downloadUrlPrefix = "https://downgit.github.io/"+downloadUrlInfix;
+                    var downloadUrlPrefix = "https://goojdex.github.io/gddowngit/"+downloadUrlInfix;
 
                     if ($routeParams.url) {
                         $scope.url = $routeParams.url;
